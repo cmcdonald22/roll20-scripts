162f439cda0ad38f44a2e74f28b809587921fbe6
joesinghaus <deceptive.duality@gmail.com>
2016-04-01 11:15:16 +0200

    Bugfix, add edit mode

--- reshaped-settings.js
+++ reshaped-settings.js
@@ -13,7 +13,8 @@ var reshapedSettings = reshapedSettings || (function() {
         'initiative_tie_breaker': '', // can be @{initiative_tie_breaker_var} or empty ('')
         'initiative_to_tracker': '@{initiative_to_tracker_yes}', // can be @{initiative_to_tracker_yes} or @{initiative_to_tracker_no}
         'attacks_vs_target_ac': '@{attacks_vs_target_ac_no}', // can be @{attacks_vs_target_ac_yes} or @{attacks_vs_target_ac_no}
-        'attacks_vs_target_name': '@{attacks_vs_target_name_no}' // can be @{attacks_vs_target_name_yes} or @{attacks_vs_target_name_no}
+        'attacks_vs_target_name': '@{attacks_vs_target_name_no}', // can be @{attacks_vs_target_name_yes} or @{attacks_vs_target_name_no}
+        'edit_mode': '0',
         'hide_attack': '@{hide_attack_var}',
         'hide_damage': '@{hide_damage_var}',
         'hide_saving_throw_failure': '@{hide_saving_throw_failure_var}',
