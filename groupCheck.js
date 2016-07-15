f37c7c6e55d8b4f89d9ff9be3d7fdc86bbe28f95
joesinghaus <deceptive.duality@gmail.com>
2016-07-15 23:15:18 +0200

    v0.5.2
    * Added multiple mods for one check & corresponding new data format.
    * Made code more readable by splitting off output line to separate function.
    * Added check to ignore tokens without id.

--- groupCheck.js
+++ groupCheck.js
@@ -1,44 +1,8 @@
 var groupCheck = groupCheck || (function() {
 	'use strict';
-	var version = '0.5',
+	var version = '0.5.2',
 	// Config Start
 	// Attribute list is for D&D 5E Shaped sheet
-	
-	attrList = {
-		'Strength Save': 'strength_saving_throw_mod',
-		'Dexterity Save': 'dexterity_saving_throw_mod',
-		'Constitution Save': 'constitution_saving_throw_mod',
-		'Intelligence Save': 'intelligence_saving_throw_mod',
-		'Wisdom Save': 'wisdom_saving_throw_mod',
-		'Charisma Save': 'charisma_saving_throw_mod',
-//		'Fortitude Save': 'fortitude_saving_throw_mod',
-//		'Reflex Save': 'reflex_saving_throw_mod',
-//		'Will Save': 'will_saving_throw_mod',
-		'Strength Check': 'strength_check_mod_formula',
-		'Dexterity Check': 'dexterity_check_mod_formula',
-		'Constitution Check': 'constitution_check_mod_formula',
-		'Intelligence Check': 'intelligence_check_mod_formula',
-		'Wisdom Check': 'wisdom_check_mod_formula',
-		'Charisma Check': 'charisma_check_mod_formula',
-		'Acrobatics': 'repeating_skill_$0_formula',
-		'Animal Handling': 'repeating_skill_$1_formula',
-		'Arcana': 'repeating_skill_$2_formula',
-		'Athletics': 'repeating_skill_$3_formula',
-		'Deception': 'repeating_skill_$4_formula',
-		'History': 'repeating_skill_$5_formula',
-		'Insight': 'repeating_skill_$6_formula',
-		'Intimidation': 'repeating_skill_$7_formula',
-		'Investigation': 'repeating_skill_$8_formula',
-		'Medicine': 'repeating_skill_$9_formula',
-		'Nature': 'repeating_skill_$10_formula',
-		'Perception': 'repeating_skill_$11_formula',
-		'Performance': 'repeating_skill_$12_formula',
-		'Persuasion': 'repeating_skill_$13_formula',
-		'Religion': 'repeating_skill_$14_formula',
-		'Sleight of Hand': 'repeating_skill_$15_formula',
-		'Stealth': 'repeating_skill_$16_formula',
-		'Survival': 'repeating_skill_$17_formula'
-	},
 	
 	die = "d20",			// Standard die to add to modifier. If constant, add 0d0 to
 							// work around sendChat bug. E.g. 0d0 + 10.
@@ -47,6 +11,42 @@ var groupCheck = groupCheck || (function() {
 	rollTwice = false,		// Always roll two dice.
 	useRollSetting = false, // Use 5E Shaped integrated roll setting. If both this and
 							// rollTwice are true, we will default to rollTwice.
+		
+	attrList = {
+		'Strength Save': ['strength_saving_throw_mod'],
+		'Dexterity Save': ['strength_saving_throw_mod'],
+		'Constitution Save': ['constitution_saving_throw_mod'],
+		'Intelligence Save': ['intelligence_saving_throw_mod'],
+		'Wisdom Save': ['wisdom_saving_throw_mod'],
+		'Charisma Save': ['charisma_saving_throw_mod'],
+//		'Fortitude Save': ['fortitude_saving_throw_mod'],
+//		'Reflex Save': ['reflex_saving_throw_mod'],
+//		'Will Save': ['will_saving_throw_mod'],
+		'Strength Check': ['strength_check_mod_formula'],
+		'Dexterity Check': ['dexterity_check_mod_formula'],
+		'Constitution Check': ['constitution_check_mod_formula'],
+		'Intelligence Check': ['intelligence_check_mod_formula'],
+		'Wisdom Check': ['wisdom_check_mod_formula'],
+		'Charisma Check': ['charisma_check_mod_formula'],
+		'Acrobatics': ['repeating_skill_$0_formula'],
+		'Animal Handling': ['repeating_skill_$1_formula'],
+		'Arcana': ['repeating_skill_$2_formula'],
+		'Athletics': ['repeating_skill_$3_formula'],
+		'Deception': ['repeating_skill_$4_formula'],
+		'History': ['repeating_skill_$5_formula'],
+		'Insight': ['repeating_skill_$6_formula'],
+		'Intimidation': ['repeating_skill_$7_formula'],
+		'Investigation': ['repeating_skill_$8_formula'],
+		'Medicine': ['repeating_skill_$9_formula'],
+		'Nature': ['repeating_skill_$10_formula'],
+		'Perception': ['repeating_skill_$11_formula'],
+		'Performance': ['repeating_skill_$12_formula'],
+		'Persuasion': ['repeating_skill_$13_formula'],
+		'Religion': ['repeating_skill_$14_formula'],
+		'Sleight of Hand': ['repeating_skill_$15_formula'],
+		'Stealth': ['repeating_skill_$16_formula'],
+		'Survival': ['repeating_skill_$17_formula']
+	},	
 	
 	// Config End
 	
@@ -101,8 +101,60 @@ var groupCheck = groupCheck || (function() {
 		return;
 	},
 	
+	addCharacterToOutput = function(id, rollSetting, roll2, dieUsed, attrMods, rollAppendix) {
+	
+		var token, character, characterId, roll2Once = false, name, totalMod = ``, output = ``;
+		
+		token = getObj('graphic', id);
+		if (token) {
+			characterId = token.get("represents");
+			if (characterId) {
+				character = getObj("character", characterId);
+				if (rollSetting) {
+					switch(getAttrByName(characterId,"roll_setting")) {
+						case "{{ignore=[[0" :
+							dieUsed = die;
+							rollAppendix = ``;
+							break;
+						case "adv {{ignore=[[0":
+							dieUsed = "2d20kh1";
+							rollAppendix = ` (Advantage)`;
+							break;
+						case "dis {{ignore=[[0" :
+							dieUsed = "2d20kl1";
+							rollAppendix = ` (Disadvantage)`;
+							break;
+						default:
+							dieUsed = die;
+							rollAppendix = ``;
+							roll2Once = true;
+					} 
+				}
+				if (useTokenName) {
+					name = token.get("name");
+				}
+				else {
+					name = character.get("name");
+				}
+			
+				for (var s in attrMods) {
+					totalMod += ` + @{${character.get("name")}|${attrMods[s]}}`;
+				}
+		
+				if (roll2 || roll2Once) {
+					output += `<p><b>${name}:</b> [[${dieUsed} ${totalMod}]]`;
+					output += ` | [[${dieUsed} ${totalMod}]]</p>`;
+					roll2Once = false;
+				} else {
+					output += `<p><b>${name}:</b> [[${dieUsed} ${totalMod}]]${rollAppendix}</p>`;
+				}
+			}
+		}
+		return output;
+	},
+	
 	handleInput = function(msg) {
-		var args, opts, token, character, characterId, attr, attrMod, name, dieUsed, rollTwoOnce, RSAppendix;
+		var args, opts = {}, attr, attrMods, dieUsed = die, rollAppendix = ``, output = ``;
 	
 		if (msg.type !== "api") {
 			return;
@@ -111,7 +163,6 @@ var groupCheck = groupCheck || (function() {
 		args = msg.content.split(/\s+--/);
 		switch(args.shift()) {
 			case '!group-check':
-				opts = {};
 				for (var arg in args) {
 					opts[args[arg]] = true;
 				}
@@ -124,7 +175,7 @@ var groupCheck = groupCheck || (function() {
 				for (var s in attrList) {
 					if (opts[s]) {
 						attr = s;
-						attrMod = attrList[s];
+						attrMods = attrList[s];
 					}
 				}
 				
@@ -133,24 +184,21 @@ var groupCheck = groupCheck || (function() {
 					return;
 				}
 				
-				var output = ``;
 				if ((whisperToGM || opts.GM) && !opts.Public) {
 					output += `/w GM `;
 				}
 				
-				output += `<div style=\"border: 1px solid black; background-color: #FFFFFF; padding: 3px 3px;\">`;
-				output += `<h3>${attr}:</h3>`;
+				output += `<div style="border: 1px solid black; background-color: #FFFFFF; padding: 3px 3px;">`;
+				output += `<h3>${attr}</h3>`;
 				output += `<br>`;
 				
-				dieUsed = die;
-				RSAppendix = ``;
 				if (opts.adv && !opts.roll2) {
 					dieUsed = "2d20kh1";
-					RSAppendix = ` (Advantage)`;
+					rollAppendix = " (Advantage)";
 					
 				} else if (opts.disadv && !opts.roll2) {
 					dieUsed = "2d20kl1";
-					RSAppendix = ` (Disadvantage)`;
+					rollAppendix = " (Disadvantage)";
 				}
 				
 				var rollSetting = (useRollSetting || opts.rollsetting) && !opts.roll2 && !opts.disadv && !opts.adv;
@@ -158,45 +206,7 @@ var groupCheck = groupCheck || (function() {
 				
 				if (msg.selected && msg.selected.length) {
 					for (var sel in msg.selected) {						   
-						token = getObj('graphic', msg.selected[sel]._id);
-						characterId = token.get("represents");
-					
-						if (characterId) {
-							character = getObj("character", characterId);
-							if (rollSetting) {
-								switch(getAttrByName(characterId,"roll_setting")) {
-									case `{{ignore=[[0` :
-										dieUsed = die;
-										RSAppendix = ``;
-										break;
-									case `adv {{ignore=[[0`:
-										dieUsed = "2d20kh1";
-										RSAppendix = ` (Advantage)`;
-										break;
-									case `dis {{ignore=[[0` :
-										dieUsed = "2d20kl1";
-										RSAppendix = ` (Disadvantage)`;
-										break;
-									default:
-										dieUsed = die;
-										RSAppendix = ``;
-										rollTwoOnce = true;
-								} 
-							}
-							if (useTokenName) {
-								name = token.get("name");
-							}
-							else {
-								name = character.get("name");
-							}
-							if (roll2 || rollTwoOnce) {
-								output += `<p><b>${name}:</b> [[${dieUsed} + @{${character.get("name")}|${attrMod}}]]`;
-								output += ` | [[${dieUsed} + @{${character.get("name")}|${attrMod}}]]</p>`;
-								rollTwoOnce = false;
-							} else {
-								output += `<p><b>${name}:</b> [[${dieUsed} + @{${character.get("name")}|${attrMod}}]]${RSAppendix}</p>`;
-							}
-						} 
+						output += addCharacterToOutput(msg.selected[sel]._id, rollSetting, roll2, dieUsed, attrMods, rollAppendix);
 					}
 				}
 				
