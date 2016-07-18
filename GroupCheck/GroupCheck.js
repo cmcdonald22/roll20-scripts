12c013eba2d070972edac0ad34d139ca3f9ed017
joesinghaus <deceptive.duality@gmail.com>
2016-07-18 23:06:00 +0200

    GroupCheck v0.6
    * added fallback, custom, globalmod, and die options
    * fixed dexterity saves
    * removed noarchive from every sendChat call

--- GroupCheck/GroupCheck.js
+++ GroupCheck/GroupCheck.js
@@ -1,11 +1,13 @@
 var groupCheck = groupCheck || (function() {
-	'use strict';
-	var version = '0.5.3',
+    'use strict';
+    var version = '0.6',
 	// Config Start
 	// Attribute list is for D&D 5E Shaped sheet
 	
 	die = "d20",			// Standard die to add to modifier. If constant, add 0d0 to
 							// work around sendChat bug. E.g. 0d0 + 10.
+	die_adv = "2d20kh1",	// Die for advantage
+	die_dis = "2d20kl1",	// Die for disadvantage
 	whisperToGM = false,	// Whisper results to GM or make them public by default.
 	useTokenName = true,	// Uses name of the token if true, character name if false.
 	alwaysRoll2 = false,	// Always roll two dice.
@@ -15,7 +17,7 @@ var groupCheck = groupCheck || (function() {
 
 	attrList = {
 		'Strength Save': ['strength_saving_throw_mod'],
-		'Dexterity Save': ['strength_saving_throw_mod'],
+		'Dexterity Save': ['dexterity_saving_throw_mod'],
 		'Constitution Save': ['constitution_saving_throw_mod'],
 		'Intelligence Save': ['intelligence_saving_throw_mod'],
 		'Wisdom Save': ['wisdom_saving_throw_mod'],
@@ -71,7 +73,7 @@ var groupCheck = groupCheck || (function() {
 			helpString += `<b>${s}</b>, `
 		}
 		helpString += "</p></div>";
-		sendChat(who, helpString, null, {noarchive:true});
+		sendChat(who, helpString);
 	},
 	
 	handleError = function(who, errorMsg, opts) {
@@ -81,7 +83,7 @@ var groupCheck = groupCheck || (function() {
 		output += "<p>"+errorMsg+"</p>";
 		output += "Input was: <p>" + JSON.stringify(opts) + "</p>";
 		output += "</div>";
-		sendChat(who, output, null, {noarchive:true});
+		sendChat(who, output);
 	},
 
 	printCommandMenu = function(who, opts) {
@@ -98,15 +100,13 @@ var groupCheck = groupCheck || (function() {
 			commandOutput += `[${s}](!group-check ${optsCommand}--${s})`;
 		}
 		commandOutput += `</div>`;
-		sendChat(who, commandOutput, null, {noarchive:true});
+		sendChat(who, commandOutput);
 		return;
 	},
 	
-	addCharacterToOutput = function(id, rollSetting, roll2, dieUsed, attrMods, rollAppendix, rollPre, rollPost) {
-	
+	addCharacterToOutput = function(tokenId, rollSetting, roll2, dieUsed, attrMods, rollAppendix, rollPre, rollPost, fallback, globalMod) {
 		var token, character, characterId, roll2Once = false, name, totalMod = ``, output = ``;
-		
-		token = getObj('graphic', id);
+		token = getObj('graphic', tokenId);
 		if (token) {
 			characterId = token.get("represents");
 			if (characterId) {
@@ -114,20 +114,16 @@ var groupCheck = groupCheck || (function() {
 				if (rollSetting) {
 					switch(getAttrByName(characterId,"roll_setting")) {
 						case "{{ignore=[[0" :
-							dieUsed = die;
-							rollAppendix = ``;
 							break;
 						case "adv {{ignore=[[0":
-							dieUsed = "2d20kh1";
+							dieUsed = die_adv;
 							rollAppendix = ` (Advantage)`;
 							break;
 						case "dis {{ignore=[[0" :
-							dieUsed = "2d20kl1";
+							dieUsed = die_dis;
 							rollAppendix = ` (Disadvantage)`;
 							break;
 						default:
-							dieUsed = die;
-							rollAppendix = ``;
 							roll2Once = true;
 					} 
 				}
@@ -141,13 +137,31 @@ var groupCheck = groupCheck || (function() {
 				for (var s in attrMods) {
 					totalMod += ` + @{${character.get("name")}|${attrMods[s]}}`;
 				}
+				if (globalMod) {
+					totalMod += ` + ${globalMod}[global modifier]`;
+				}
 		
 				if (roll2 || roll2Once) {
 					output += `<p><b>${name}:</b> ${rollPre}${dieUsed} ${totalMod}${rollPost}`;
 					output += ` | ${rollPre}${dieUsed} ${totalMod}${rollPost}</p>`;
-					roll2Once = false;
 				} else {
 					output += `<p><b>${name}:</b> ${rollPre}${dieUsed} ${totalMod}${rollPost}${rollAppendix}</p>`;
+				}
+			}
+			else if (fallback) {					// Do this if token does not represent a character and fallback is active
+				if (token.get("name") !== '') {
+					name = token.get("name");
+				} else {
+					name = "Default";
+				}
+				if (globalMod) {
+					fallback += ` + ${globalMod}[global modifier]`;
+				}
+				if (roll2) {
+					output += `<p><b>${name}:</b> ${rollPre}${dieUsed} + ${fallback}${rollPost}`;
+					output += ` | ${rollPre}${dieUsed} + ${fallback}${rollPost}</p>`;
+				} else {
+					output += `<p><b>${name}:</b> ${rollPre}${dieUsed} + ${fallback}${rollPost}${rollAppendix}</p>`;	
 				}
 			}
 		}
@@ -165,15 +179,23 @@ var groupCheck = groupCheck || (function() {
 		args = msg.content.split(/\s+--/);
 		switch(args.shift()) {
 			case '!group-check':
-				for (var arg in args) {
-					opts[args[arg]] = true;
+				// Read options
+				for (var k in args) {
+					var kv = args[k].split(/\s(.+)/);
+					if (kv[1] && (kv[0] === 'fallback' || kv[0] === 'custom' || kv[0] === 'die' || kv[0] === 'globalmod')) {
+						opts[kv[0]] = kv[1];
+					} else {
+						opts[args[k]] = true;
+					}
 				}
-
+				
+				// Help
 				if (opts.help) {
 					printHelp(msg.who);
 					return;
 				}
 				
+				// Look for attribute supplied
 				for (var s in attrList) {
 					if (opts[s]) {
 						attr = s;
@@ -181,28 +203,48 @@ var groupCheck = groupCheck || (function() {
 					}
 				}
 				
-				if (!attr) {
+				// Handle custom modifier
+				if (opts.custom) {
+					var kv = opts.custom.split(/\s*,\s*/);
+					if (kv.length < 2) {
+						handleError(msg.who,"Custom roll format invalid", args);
+						return;
+					}
+					attr = kv.shift();
+					attrMods = kv;
+				}
+				
+				// Print menu if we don't know what to roll
+				if (!attr && !opts.custom) {
 					printCommandMenu(msg.who,opts);
 					return;
 				}
 				
+				// Whisper output if desired
 				if ((whisperToGM || opts.GM) && !opts.Public) {
 					output += `/w GM `;
 				}
 				
+				// Output preamble
 				output += `<div style="border: 1px solid black; background-color: #FFFFFF; padding: 3px 3px;">`;
 				output += `<h3>${attr}</h3>`;
 				output += `<br>`;
 				
+				// Handle custom die. Ignored if rolling with advantage or disadvantage.
+				if (opts.die) {
+					dieUsed = opts.die;
+				}
+				
+				// Handle advantage and disadvantage.
 				if (opts.adv && !opts.roll2) {
-					dieUsed = "2d20kh1";
+					dieUsed = die_adv;
 					rollAppendix = " (Advantage)";
-					
 				} else if (opts.disadv && !opts.roll2) {
-					dieUsed = "2d20kl1";
+					dieUsed = die_dis;
 					rollAppendix = " (Disadvantage)";
 				}
 				
+				// Handle bonus hiding
 				if ((hideBonus && !opts.showbonus) || opts.hidebonus) {
 					rollPre= '[[[[';
 					rollPost= ']]]]';
@@ -213,13 +255,12 @@ var groupCheck = groupCheck || (function() {
 				
 				if (msg.selected && msg.selected.length) {
 					for (var sel in msg.selected) {						   
-						output += addCharacterToOutput(msg.selected[sel]._id, rollSetting, roll2, dieUsed, attrMods, rollAppendix, rollPre, rollPost);
+						output += addCharacterToOutput(msg.selected[sel]._id, rollSetting, roll2, dieUsed, attrMods, rollAppendix, rollPre, rollPost, opts.fallback, opts.globalmod);
 					}
 				}
 				
 				output += `</div>`;
 				sendChat(msg.who, output);
-				return;
 		} 
 		return; 
 	},
---
+++ GroupCheck/README.md
@@ -0,0 +1,60 @@
+# GroupCheck
+A roll20 API script for rolling multiple checks at once.
+
+## Basic usage
+The script is preloaded to come with commands for the D&D 5E Shaped Sheet. See the next chapter to learn how you can supply the necessary info to run it with another character sheet. The basic syntax is 
+
+		!group-check [--options] [--Check Name]
+		
+Here, you can supply zero or more options (see below for specifics) that modify what exactly is rolled. **Check Name** is the name of the check as supplied in the **attrList** variable. If no valid **Check Name** is supplied, a list of valid checks (in the form of API buttons) is instead output to chat, allowing you to press them to roll the corresponding check.
+**Check Name** will then be rolled once for every selected token that represents a character, and the result will be output to chat.
+
+### Example
+Suppose that we are using D&D 5E, and want to roll a Dexterity saving throw for every selected token, outputting the result to the GM only. The command would be
+
+		!group-check --GM --Dexterity Save 
+This refers to the line
+
+		'Dexterity Save' : ['dexterity_saving_throw_mod'],
+in the **attrList** variable.
+If you have two tokens selected, representing the characters **Sarah** and **Mark**, the script will output (with default settings)
+
+**Sarah:** [[d20 + @{Sarah|dexterity\_saving\_throw\_mod}]]
+
+**Mark:** [[d20 + @{Mark|dexterity\_saving\_throw\_mod}]]
+
+## Configuration options
+The script is designed to be easily configured to your specific system's needs.
+
+### Modifying attributes
+The standard list of checks and corresponding modifiers is contained in the **attrList** variable at the beginning of the script. Modify it to adapt the script to your needs. The basic format is
+
+		attrList = {
+			'Check_1 Name' : ['check_1_mod_1', ... , 'check_1_mod_n'],
+			...
+			'Check_m Name' : ['check_m_mod_1', ... , 'check_m_mod_l']
+		},
+
+The names both serve both as titles for the roll output and as options needed to run these checks. The mods are names of character attributes; all attributes of the selected characters with these names are added together to get the final modifier to the **die** roll. The following strings are not available as check names, because they're used as command line options: **Public, GM, adv, disadv, rollsetting, roll2, showbonus,** and **hidebonus**. Similarly, check names may not start with any of **fallback, custom, die,** or **globalmod**, followed by a space. (and naturally, they may not contain double dashes (**--**), although this is unlikely to be a problem).
+
+### Roll options
+There are two ways of customising the way the script runs: through static config variables set at the start of the script's code, or via command line options. Command line options, if supplied, always override the behaviour set in the config variables.
+
+* The variables **die**, **die_adv**, and **die_dis** control the basic dice used for normal rolling, disadvantage, and advantage. Supplying the option **--die [dieSize]** will use **[dieSize]** instead the default die; this is ignored when rolling with advantage or disadvantage.
+
+* The variable **whisperToGM** controls if rolls are whispered to the GM or output publicly. The corresponding command line options are **--Public** and **--GM**.
+
+* The variable **useTokenName** controls if the name of the token or the name of the character is displayed in front of the roll result. You can use e.g. the TokenNameNumber script to give different tokens for the same character different (numbered) names, allowing you to discern which of the tokens rolled which roll, even if there are several tokens representing the same character. This is active by default.
+
+* It is possible to alter the specific way rolls are made. There are 5 options: roll normally, roll with advantage, roll with disadvantage, always roll 2 times for every token, or (for the 5E shaped sheet only) respect the roll setting on the sheet for selected tokens. The corresponding config options are 
+*alwaysRoll2* and *useRollSetting*, the command line options are **--adv**, **--disadv**, **--rollsetting**, and **--roll2**.
+
+* The option **--globalmod [mod]** will add **[mod]** as a modifier to all rolls made. Here **[mod]** can be any expression that the roll20 dice roller can interpret, such as a number, a die roll, a specific character's attribute, or a sum of these things.
+
+* It is possible to hide the bonus on rolls and only show the final result of the roll. This is controlled via **hideBonus** and the command line options **--showbonus** and **--hidebonus**.
+
+* You can supply a fallback value. When the option **--fallback [value]** is given, a roll will be made even for tokens not linked to a character; for these tokens, **[value]** is used instead of a character's attribute as modifier to the roll. **[value]** may be any expression that the roll20 dice roller can interpret, such as a number, a die roll, a specific character's attribute, or a sum of these things. If also using **--globalmod**, the global modifier is applied in addition to the fallback mod.
+
+* (Advanced) It is possible to supply a custom roll not present in **attrList**. The syntax to do this is **--custom CheckName, mod\_1, ..., mod\_n**. This will roll a check with title **CheckName** and modifiers **mod\_1**, **mod\_2**, and so on. This works exactly as if there was an extra line in **attrList** as follows.
+
+		'CheckName' : ['mod_1', 'mod_2', ... 'mod_n'],
