4c3747bfbc1deb740c53075d823d0bf05c33fa10
joesinghaus <deceptive.duality@gmail.com>
2016-07-20 15:54:07 +0200

    ChatSetAttr v0.4
    Rewriting almost everything.

--- ChatSetAttr/ChatSetAttr.js
+++ ChatSetAttr/ChatSetAttr.js
@@ -1,12 +1,12 @@
-// Example Usage:	!setattr --Str : 14, AC:12, HP : 50
-// or				!setattr --charid @{John|character_id} --Wits: 15, Perception: 27
+// Example Usage:	!setattr --Str | 14, AC|12, HP | 50 --Juice |12
+// or				!setattr --charid @{John|character_id}, @{Mark|character_id} --Wits| 15, Perception|27 
 //
 // Set feedback to false to disable output (except for error messages).
 
 var chatSetAttr = chatSetAttr || (function() {
     'use strict';
 	
-	var version = '0.3',
+	var version = '0.4',
 	feedback = true,
 	
 	checkInstall = function() {
@@ -22,8 +22,9 @@ var chatSetAttr = chatSetAttr || (function() {
 		output += "</div>";
 		sendChat(who, output);
 	},
-
-	myGetAttrByName = function(character_id, attribute_name,attribute_default_current, attribute_default_max) {
+	
+	myGetAttrByName = function(character_id, attribute_name, attribute_default_current, attribute_default_max) {
+		// Returns attribute object by name
 		attribute_default_current = attribute_default_current || '';
 		attribute_default_max = attribute_default_max || '';
 
@@ -43,116 +44,131 @@ var chatSetAttr = chatSetAttr || (function() {
 		return attribute;
 	},
 
-	setAttributes = function(character_id, setting) {
-		for (var k in setting) {
-			 myGetAttrByName(character_id, k).set("current", setting[k]);
+	processInlinerolls = function (msg) {
+		// Input:	msg - chat message
+		// Output:	msg.content, with all inline rolls evaluated
+		if (_.has(msg, 'inlinerolls')) {
+			return _.chain(msg.inlinerolls)
+					.reduce(function(previous, current, index) {
+						previous['$[[' + index + ']]'] = current.results.total || 0;
+						return previous;
+					},{})
+					.reduce(function(previous, current, index) {
+						return previous.replace(index, current);
+					}, msg.content)
+					.value();
+		} else {
+			return msg.content;
 		}
+	},
+	
+	setAttributes = function(list, setting) {
+		// Input:	list - array of valid character IDs
+		//			setting - object containing attribute names and desired values
+		// Output:	null. Attribute values are changed.
+		list.forEach(function(c) {
+			_.each(setting, function(s,t) {
+				myGetAttrByName(c,t).set("current",s);
+			});
+		});
 		return;
 	},
-		
-	handleInput = function(msg) {
-		if (msg.type !== "api") {
-			return;
+	
+	processOpts = function(content, hasValue) {
+		// Input:	content - string of the form command --opts1 --opts2  value --opts3.
+		//					values come separated by whitespace.
+		//			hasValue - array of all options which come with a value
+		// Output:	object containing key:true if key is not in hasValue. and containing
+		//			key:value otherwise
+		var args, kv, opts = {};
+		args = _.rest(content.split(/\s+--/));
+		for (var k in args) {
+			kv = args[k].split(/\s(.+)/);
+			if (_.contains(hasValue, kv[0])) {
+				opts[kv[0]] = kv[1];	
+			}
+			else {
+				opts[args[k]] = true;	
+			}
 		}
-		var args, attr, cmd, character, characterId, opts={}, output = '', setting={}, settingNames='', settingValues='', token;
+		return opts;
+	},
 
-		cmd = msg.content.split(/\s+--/);
-		if (cmd.shift() === '!setattr') {
+	parseAttributes = function(args) {
+		// Input:	args - array containing comma-separated list of strings, every one of which contains
+		// 			an expression of the form key|value
+		// Output:	Object containing key|value for all expressions.
+		return _.chain(args)
+		.map(function(str){return str.split(/\s*,\s*/);})
+		.flatten()
+		.map(function(str){return str.split(/\s*\|\s*/);})
+		.object()
+		.value();
+	},
+
+	handleInput = function(msg) {
+		var charIDList = [], opts, setting;
+		var optsArray = ['charid'];
+		
+		if (msg.type === "api" && msg.content.match(/^!setattr\b/)) {
 			
-			// Some options should be provided.
-			if (!cmd[0]) {
+			// Parse input
+			opts = processOpts(processInlinerolls(msg), optsArray);
+			setting = parseAttributes(_.chain(opts).omit(optsArray).keys().value());
+			
+			if (_.isEmpty(setting)) {
 				handleError(msg.who, "No options supplied.", msg.content);
 				return;
 			}
-			
-			// Getting options
-			for (var k in cmd) {
-				if (k < cmd.length -1) {
-					var kv = cmd[k].split(/\s+/);
-					if (kv[1]) {
-						opts[kv[0]] = kv[1];
-					} else {
-						opts[kv[0]] = true;
-					}
-				}		
-			}	
-			
-			// Getting all the attributes we have to set
-			args = cmd[cmd.length - 1].split(/\s*,\s*/);		
-			for (var k in args) {
-				attr = args[k].split(/\s*:\s*/);
-				if (!attr || attr.length != 2) {
-					handleError(msg.who, "There was a problem with the input.", msg.content);
-					return;
-				}
-				setting[attr[0]] = attr[1];
-				if (feedback) {
-					if (k < args.length - 1) {
-						settingNames += `${attr[0]}, `;
-						settingValues += `${attr[1]}, `;
-					}
-					else {
-						settingNames += `${attr[0]}`;
-						settingValues += `${attr[1]}`;
+
+			// Get characters, either from charid or from selected tokens
+			if (opts.charid) {
+				charIDList = opts.charid.split(/\s*,\s*/);
+			} else if (msg.selected && msg.selected.length) {
+				let characterId, token;
+				for (var sel in msg.selected) {						   
+					token = getObj('graphic', msg.selected[sel]._id);
+					if (token) {
+						characterId = token.get("represents");
+						if (characterId) {
+							charIDList.push(characterId);	
+						}
 					}
 				}
+			} else {
+				handleError(msg.who,"No tokens selected.", msg.content);
+				return;
 			}
 
-			
-			if (opts.charid || (msg.selected && msg.selected.length)) {
-				if (feedback) {
-					output += `/w ${msg.who}`;
-					output += "<div style=\"border: 1px solid black; background-color: #FFFFFF; padding: 3px 3px;\">";
-					output += `<p>Set ${settingNames} to ${settingValues} for characters `;
-				}
-			
-				if (opts.charid) {
-					var allowed = playerIsGM(msg.playerid);
-					var character = getObj("character",opts.charid);
-					if (!character) {
-						handleError(msg.who, "A character with this id does not exist.", msg.content);
-						return;
-					}
-					if (!allowed) {
-						var contr = character.get("controlledby").split(/\s*,\s*/);
-						for (var k in contr) {
-							if (contr[k] === msg.playerid || contr[k] === 'all') allowed = true;
+			// Need to check permissions and existence of characters if charid is supplied.
+			if (opts.charid) {
+				let control, character;
+				for (var k in charIDList) {
+					character = getObj("character",charIDList[k]);
+					if (character) {
+						control = character.get('controlledby').split(/,/);
+						if(!(_.contains(control,'all') || _.contains(control,msg.playerid))) { //playerIsGM(msg.playerid) || 
+							charIDList.splice(k,1);
+							handleError(msg.who, "Permission error.", msg.content);
 						}
-					}
-					if (allowed) {
-						setAttributes(opts.charid, setting);
-						output += character.get("name");
-					}
-					else {
-						handleError(msg.who, "You do not have permission.", msg.content);
-						return;
-					}
-				} else {
-					for (var sel in msg.selected) {						   
-						token = getObj('graphic', msg.selected[sel]._id);
-						if (token) {
-							characterId = token.get("represents");
-							if (characterId) {
-								setAttributes(characterId, setting);
-								if (feedback) {
-									if (sel < msg.selected.length - 1) {
-										output += getAttrByName(characterId, "character_name") + ", ";
-									} else {
-										output += getAttrByName(characterId, "character_name");
-									}
-								}
-							}
-						}
-					}
+					} else {
+						charIDList.splice(k,1);
+						handleError(msg.who, "Invalid character id.", msg.content);
+					}	
 				}
-				
-				if (feedback) {
-					output += ".</p></div>";
-					sendChat(msg.who, output);
-				}
-			}
-			else {
-				handleError(msg.who,"No tokens selected", msg.content);
+			}			
+			
+			// Set attributes
+			setAttributes(charIDList,setting);
+			
+			// Output
+			if (feedback && !opts.silent) {
+				var charNames = charIDList.map(function(id) {return getAttrByName(id, "character_name")}).join(", ");
+				var output = `/w ${msg.who}` +
+					`<div style="border: 1px solid black; background-color: #FFFFFF; padding: 3px 3px;">` +
+					`<p>Setting ${_.keys(setting).join(", ")} to ${_.values(setting).join(", ")} ` +
+					`for characters ${charNames}.</p></div>`;
+				sendChat(msg.who, output);
 			}
 		}
 		return;
