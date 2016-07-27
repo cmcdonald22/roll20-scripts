66aa498d43f254f9fafc528937e55bf50b28ad1e
joesinghaus <deceptive.duality@gmail.com>
2016-07-27 13:42:20 +0200

    ChatSetAttr v0.5
    Added —gm, —allgm options and juggled around a lot of the code.

--- ChatSetAttr/ChatSetAttr.js
+++ ChatSetAttr/ChatSetAttr.js
@@ -1,34 +1,34 @@
 // Example Usage:	!setattr --Str | 14, AC|12, HP | 50 --Juice |12
-// or				!setattr --charid @{John|character_id}, @{Mark|character_id} --Wits| 15, Perception|27 
+// or				!setattr --charid @{John|character_id}, @{Mark|character_id} --Wits| 15, Perception|27
 //
 // Set feedback to false to disable output (except for error messages).
 
 var chatSetAttr = chatSetAttr || (function() {
     'use strict';
-	
-	var version = '0.4',
+
+	const version = '0.5',
 	feedback = true,
-	
+
 	checkInstall = function() {
-		log(` -=> ChatSetAttr v${version} <=-`);
+		log(`-=> ChatSetAttr v${version} <=-`);
 	},
 
 	handleError = function(who, errorMsg, cmd) {
-		var output = "/w " + who;
-		output += "<div style=\"border: 1px solid black; background-color: #FFBABA; padding: 3px 3px;\">";
-		output += "<h4>Error</h4>";
-		output += "<p>"+errorMsg+"</p>";
-		output += "Input was: <p>" + cmd + "</p>";
-		output += "</div>";
+		let output = "/w " + who
+			+ "<div style=\"border: 1px solid black; background-color: #FFBABA; padding: 3px 3px;\">"
+			+ "<h4>Error</h4>"
+			+ "<p>"+errorMsg+"</p>"
+			+ "Input was: <p>" + cmd + "</p>"
+			+ "</div>";
 		sendChat(who, output);
 	},
-	
+
 	myGetAttrByName = function(character_id, attribute_name, attribute_default_current, attribute_default_max) {
 		// Returns attribute object by name
 		attribute_default_current = attribute_default_current || '';
 		attribute_default_max = attribute_default_max || '';
 
-		var attribute = findObjs({
+		let attribute = findObjs({
 			type: 'attribute',
 			characterid: character_id,
 			name: attribute_name
@@ -61,7 +61,7 @@ var chatSetAttr = chatSetAttr || (function() {
 			return msg.content;
 		}
 	},
-	
+
 	setAttributes = function(list, setting) {
 		// Input:	list - array of valid character IDs
 		//			setting - object containing attribute names and desired values
@@ -74,22 +74,22 @@ var chatSetAttr = chatSetAttr || (function() {
 		});
 		return;
 	},
-	
+
 	processOpts = function(content, hasValue) {
 		// Input:	content - string of the form command --opts1 --opts2  value --opts3.
 		//					values come separated by whitespace.
 		//			hasValue - array of all options which come with a value
 		// Output:	object containing key:true if key is not in hasValue. and containing
 		//			key:value otherwise
-		var args, kv, opts = {};
+		let args, kv, opts = {};
 		args = _.rest(content.split(/\s+--/));
-		for (var k in args) {
+		for (let k in args) {
 			kv = args[k].split(/\s(.+)/);
 			if (_.contains(hasValue, kv[0])) {
-				opts[kv[0]] = kv[1];	
+				opts[kv[0]] = kv[1];
 			}
 			else {
-				opts[args[k]] = true;	
+				opts[args[k]] = true;
 			}
 		}
 		return opts;
@@ -99,34 +99,45 @@ var chatSetAttr = chatSetAttr || (function() {
 		// Input:	args - array containing comma-separated list of strings, every one of which contains
 		// 			an expression of the form key|value
 		// Output:	Object containing key|value for all expressions.
+		//
+		// It can deal with empty values
 		return _.chain(args)
-		.map(function(str){return str.split(/\s*,\s*/);})
+		.map(str => str.split(/\s*,\s*/))
 		.flatten()
-		.map(function(str){return str.split(/\s*\|\s*/);})
+		.map(str => str.split(/\s*\|\s*/))
 		.object()
 		.value();
 	},
 
 	handleInput = function(msg) {
-		var charIDList = [], opts, setting;
-		var optsArray = ['charid'];
-		
+		let charIDList;
+		const hasValue = ['charid'], optsArray = ['all','allgm','charid','silent'];
+
 		if (msg.type === "api" && msg.content.match(/^!setattr\b/)) {
-			
+
 			// Parse input
-			opts = processOpts(processInlinerolls(msg), optsArray);
-			setting = parseAttributes(_.chain(opts).omit(optsArray).keys().value());
-			
+			const opts = processOpts(processInlinerolls(msg), hasValue);
+			const setting = parseAttributes(_.chain(opts).omit(optsArray).keys().value());
+
 			if (_.isEmpty(setting)) {
 				handleError(msg.who, "No options supplied.", msg.content);
 				return;
 			}
 
 			// Get characters, either from charid or from selected tokens
-			if (opts.charid) {
+			if (opts.all && playerIsGM(msg.playerid)) {
+				charIDList = _.map(findObjs({_type: 'character'}), c => c.id);
+			}
+			else if (opts.allgm && playerIsGM(msg.playerid)) {
+				charIDList = _.chain(findObjs({_type: 'character'}))
+							.filter(c => c.get('controlledby') === '')
+							.map(c => c.id)
+							.value();
+			}
+			else if (opts.charid) {
 				charIDList = opts.charid.split(/\s*,\s*/);
 				let control, character;
-				for (var k in charIDList) {
+				for (let k in charIDList) {
 					character = getObj("character",charIDList[k]);
 					if (character) {
 						control = character.get('controlledby').split(/,/);
@@ -134,34 +145,38 @@ var chatSetAttr = chatSetAttr || (function() {
 							charIDList.splice(k,1);
 							handleError(msg.who, "Permission error.", msg.content);
 						}
-					} else {
+					}
+					else {
 						charIDList.splice(k,1);
 						handleError(msg.who, "Invalid character id.", msg.content);
-					}	
+					}
 				}
-			} else if (msg.selected && msg.selected.length) {
+			}
+			else if (msg.selected && msg.selected.length) {
+				charIDList = [];
 				let characterId, token;
-				for (var sel in msg.selected) {						   
+				for (let sel in msg.selected) {
 					token = getObj('graphic', msg.selected[sel]._id);
 					if (token) {
 						characterId = token.get("represents");
 						if (characterId) {
-							charIDList.push(characterId);	
+							charIDList.push(characterId);
 						}
 					}
 				}
-			} else {
+			}
+			else {
 				handleError(msg.who,"No tokens selected.", msg.content);
 				return;
-			}			
-			
+			}
+
 			// Set attributes
 			setAttributes(charIDList,setting);
-			
+
 			// Output
 			if (feedback && !opts.silent) {
-				var charNames = charIDList.map(function(id) {return getAttrByName(id, "character_name")}).join(", ");
-				var output = `/w ${msg.who}` +
+				let charNames = charIDList.map(id => getAttrByName(id, "character_name")).join(", ");
+				let output = `/w ${msg.who}` +
 					`<div style="border: 1px solid black; background-color: #FFFFFF; padding: 3px 3px;">` +
 					`<p>Setting ${_.keys(setting).join(", ")} to ${_.values(setting).join(", ")} ` +
 					`for characters ${charNames}.</p></div>`;
